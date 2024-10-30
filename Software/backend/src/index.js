aa9a3defac6aa8e43f8e336d08a3fd423140b88d
Joaqomv <93748512+Joaqomv@users.noreply.github.com>
2024-10-29 21:39:25 -0300

    eliminacion de codigos basura

--- Software/backend/src/index.js
+++ Software/backend/src/index.js
@@ -11,7 +11,7 @@ import { connectDB } from "./config/configDb.js";
 import { createInitialData } from "./config/initialSetup.js";
 import { passportJwtSetup } from "./auth/passport.auth.js";
 import ingredienteRoutes from "./routes/ingrediente.routes.js"; // Importar las rutas de Ingredientes
-import IngrechefRoutes from "./routes/Ingrechef.routes.js";
+
 
 async function setupServer() {
   try {
--- Software/backend/src/routes/Ingrechef.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { checkDishPreparation } from "../controllers/Ingrechef.controller.js";
-
-const router = Router();
-
-router.post("/check-dish", checkDishPreparation);
-
-export default router;
