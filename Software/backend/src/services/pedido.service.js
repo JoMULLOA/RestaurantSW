f77f7e133200b7bd270ae2cfcc2784a8b5445993
Joaqomv <93748512+Joaqomv@users.noreply.github.com>
2024-12-11 21:27:21 -0300

    ingredientes en pedido

--- Software/backend/src/services/pedido.service.js
+++ Software/backend/src/services/pedido.service.js
@@ -2,6 +2,11 @@
 import { AppDataSource } from "../config/configDb.js";
 import pedido from "../entity/pedido.entity.js";
 
+//j
+import { getMenus } from "./menu.service.js";
+import ingrediente from "../entity/ingrediente.entity.js";
+import { getIngredientes } from "./ingrediente.service.js";
+
 export const getPedidos = async () => {
   const pedidoRepository = AppDataSource.getRepository(pedido);
   return await pedidoRepository.find();
@@ -12,6 +17,40 @@ export const addPedido = async (data) => {
     const newPedido = pedidoRepository.create(data);
     return await pedidoRepository.save(newPedido);
 };
+
+//ADD de joaquin con resta de ignredientes
+/*
+export const addPedido = async (data) => {
+  const pedidoRepository = AppDataSource.getRepository(pedido);
+  const newPedido = pedidoRepository.create(data);
+  const menus = await getMenus();
+  const ingredientesEs = await getIngredientes();
+  const ingredientRepository = AppDataSource.getRepository(ingrediente);
+  console.log("Pedido creado:", newPedido);
+  
+  for(const plato of newPedido.plato){
+    const menu = menus.find(a => a.nombre === plato);
+    if (!menu) {
+        console.log(`No se encontró el menú para el plato: ${plato}`);
+        continue;
+    }
+    for(const ingrediente of menu.ingredientes){
+      const ingre = ingredientesEs.find(ing => ing.nombre === ingrediente.nombre);
+      if (!ingre) {
+          console.log(`No se encontró el ingrediente: ${ingrediente.nombre}`);
+          continue;
+      }
+      if (ingre.cantidad >= ingrediente.cantidad) {
+          ingre.cantidad -= ingrediente.cantidad; // Actualiza la cantidad
+          await ingredientRepository.save(ingre); // Guarda el cambio en la base de datos
+      } else { 
+          console.log(`No hay suficiente cantidad para el ingrediente: ${ingrediente.nombre}`);
+      }
+    }
+  }
+  return await pedidoRepository.save(newPedido);
+
+}*/
 
 export const removePedido = async (id) => {
   const pedidoRepository = AppDataSource.getRepository(pedido);
