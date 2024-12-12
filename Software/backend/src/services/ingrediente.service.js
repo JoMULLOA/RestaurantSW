3d757c119cf18934e0a4e25b65a2f54b8ce3f0eb
KrozJGG <christian.jamett2101@alumnos.ubiobio.cl>
2024-12-11 22:37:14 -0300

    ADD implementar validation MOD service

--- Software/backend/src/services/ingrediente.service.js
+++ Software/backend/src/services/ingrediente.service.js
@@ -8,11 +8,15 @@ export const getIngredientes = async () => {
 };
 
 export const addIngrediente = async (data) => {
-    const ingredientRepository = AppDataSource.getRepository(ingrediente);
-    /*saber si existe el nombre para agregar*/
+  const ingredientRepository = AppDataSource.getRepository(ingrediente);
+  // Verificar si ya existe un ingrediente con el mismo nombre
+  const existingIngrediente = await ingredientRepository.findOneBy({ nombre: data.nombre });
+  if (existingIngrediente) {
+    throw new Error("El ingrediente ya existe");
+  }
 
-    const newIngrediente = ingredientRepository.create(data);
-    return await ingredientRepository.save(newIngrediente);
+  const newIngrediente = ingredientRepository.create(data);
+  return await ingredientRepository.save(newIngrediente);
 };
 
 export const removeIngrediente = async (id) => {
@@ -34,9 +38,14 @@ export const updateIngredienteService = async (id, cantidad) => {
   if (!ingredienteToUpdate) {
     return null;
   }
-
   ingredienteToUpdate.cantidad = cantidad;
-  await ingredientRepository.save(ingredienteToUpdate);
-  return ingredienteToUpdate;
+  //transformar a entero
+  const q = parseInt(ingredienteToUpdate.cantidad);
+  if (q < 0) {
+    console.log("La cantidad no puede ser negativa");
+    return null;
+  }else{
+    await ingredientRepository.save(ingredienteToUpdate);
+    return ingredienteToUpdate;
+  }
 };
-
