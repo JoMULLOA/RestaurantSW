328b06deb09398ecd2fff89c24c48f4b115a6ad8
Joaqomv <93748512+Joaqomv@users.noreply.github.com>
2024-12-17 21:26:41 -0300

    Suma para grafico en chef

---
+++ Software/backend/src/entity/PedidoHistory.entity.js
@@ -0,0 +1,24 @@
+import { EntitySchema } from "typeorm";
+
+const PedidoHistory = new EntitySchema({
+  name: "PedidoHistory",
+  tableName: "pedido_history",
+  columns: {
+    id: {
+      type: "int",
+      primary: true,
+      generated: true,
+    },
+    total: {
+      type: "float",
+      nullable: false,
+    },
+    fecha: {
+      type: "timestamp", // Cambiar de datetime a timestamp
+      nullable: false,
+      default: () => "CURRENT_TIMESTAMP",
+    },
+  },
+});
+
+export default PedidoHistory;
